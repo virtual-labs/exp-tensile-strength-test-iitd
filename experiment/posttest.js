0d03392555087c72a682f77f48be2fec8aac06b2
vlabsdev-iitd <vlabsdev.iitd@gmail.com>
2021-03-01 13:30:15 +0530

    split

--- experiment/posttest.js
+++ experiment/posttest.js
@@ -89,22 +89,51 @@
 
 
     const myQuestions = [{
-            question: "1.This is a sample question:", ///// Write the question inside double quotes
+            question: "1. What is the correct loading rate from the following in N/min?", ///// Write the question inside double quotes
             answers: {
-                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
-                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
+                a: "<img src='images/post1.png'/>", ///// Write the option 1 inside double quotes
+                b: "<img src='images/post2.png'/>", ///// Write the option 2 inside double quotes
+                c: "<img src='images/post3.png'/>", ///// Write the option 1 inside double quotes
+                d: "<img src='images/post4.png'/>", ///// Write the option 2 inside double quotes
             },
             correctAnswer: "a" ///// Write the correct option inside double quotes
         },
 
-    {
-      question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
-      answers: {
-        a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
-        b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
-        c: "<img src='images/1c.PNG'>",      },
-      correctAnswer: "c"                ///// Write the correct option inside double quotes
-    },
+        {
+            question: "2. The split tensile strength of the cylindrical concrete specimen is given by?", ///// Write the question inside double quotes
+            answers: {
+                a: "<img src='images/post5.png'/>", ///// Write the option 1 inside double quotes
+                b: "<img src='images/post6.png'/>", ///// Write the option 2 inside double quotes
+                c: "<img src='images/post7.png'/>", ///// Write the option 1 inside double quotes
+                d: "<img src='images/post8.png'/>", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "a" ///// Write the correct option inside double quotes
+        },
+
+        {
+            question: "3. What is the orientation of failure plane in split tensile test assuming that the compressive load on the specimen is applied vertically?", ///// Write the question inside double quotes
+            answers: {
+                a: "Horizontal", ///// Write the option 1 inside double quotes
+                b: "Vertical", ///// Write the option 2 inside double quotes
+                c: "20' from Vertical", ///// Write the option 1 inside double quotes
+                d: "45' from Vertical", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "b" ///// Write the correct option inside double quotes
+        },
+
+         
+
+         
+
+    // {
+    //   question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
+    //   answers: {
+    //     a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
+    //     b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
+    //     c: "<img src='images/1c.PNG'>",      
+// },
+    //   correctAnswer: "c"                ///// Write the correct option inside double quotes
+    // },
 
 
         
---
+++ experiment/posttest.md
@@ -0,0 +1,31 @@
+# Post Test
+Q1. What is the correct loading rate from the following in N/min?<br>
+
+**A      <img src="images/post1.png"/>**<br>
+
+B     <img src="images/post2.png"/><br>
+
+C     <img src="images/post3.png"/><br>
+
+D     <img src="images/post4.png"/><br><br>
+
+
+Q2. The split tensile strength of the cylindrical concrete specimen is given by?<br>
+
+**A      <img src="images/post5.png"/>**<br>
+
+B     <img src="images/post6.png"/><br>
+
+C     <img src="images/post7.png"/><br>
+
+D     <img src="images/post8.png"/><br><br>
+
+Q3. What is the orientation of failure plane in split tensile test assuming that the compressive load on the specimen is applied vertically?<br>
+
+A    Horizontal<br>
+
+**B    Vertical**<br>
+
+C    20' from Vertical<br>
+
+D    45' from Vertical<br><br>
